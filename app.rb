ffa8b9f38cd6a5738811a406e60659f7eccda7d4
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-04 14:48:38 -0400

    completed lab

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Michael"
+  end
+
+  get "/hometown" do
+    "My hometown is Wilmington, NC"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Appeal, Moneybagg Yo & NBA YoungBoy"
+  end
 end
