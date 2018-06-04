0154c7a0eecf91e0f42e9e98e462c6ff89093f7f
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-06-04 02:21:53 -0400

    Completed sinatra-basic-routes-lab-dumbo-web-051418

--- app.rb
+++ app.rb
@@ -1,4 +1,29 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+    <<-HEREDOC
+    Hello, World!
+
+    Available routes:
+    /name
+    /hometown
+    /favorite-song
+    HEREDOC
+  end
+
+  get '/name' do
+    "My name is Raz!"
+  end
+
+  get '/hometown' do
+    "My hometown is the Bronx."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is There are waaay to many to list..."
+  end
+
 end
